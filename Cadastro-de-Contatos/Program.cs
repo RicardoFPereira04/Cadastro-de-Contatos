98b4651e254ec8db6429b44b087f6f45fb0bb4e4
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2023-04-27 22:05:33 -0300

    tentandoresolvererros

--- Cadastro-de-Contatos/Program.cs
+++ Cadastro-de-Contatos/Program.cs
@@ -1,22 +1,15 @@
+using Cadastro_de_Contatos.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-
-    using Cadastro_de_Contatos.Data;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-
-    namespace Cadastro_de_Contatos
+namespace Cadastro_de_Contatos
+{
+    public class Program
     {
-
-
-
-
-
-        public class Program
-        {
             public static void Main(string[] args)
             {
                 var builder = WebApplication.CreateBuilder(args);
@@ -25,9 +18,14 @@
                 builder.Services.AddControllersWithViews();
 
                 var app = builder.Build();
+         
+         
+            builder.Services.AddDbContext<BancoContext>(options =>
+            options.UseSqlServer
+            ("Data Source=DESKTOP-GFK2EGS\\;Initial Catalog=TesteDb; Integrated Security=True; Encrypt=False"));
 
-                // Configure the HTTP request pipeline.
-                if (!app.Environment.IsDevelopment())
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
                 {
                     app.UseExceptionHandler("/Home/Error");
                 }
